34bf9dc1b7485a166888d6550cb3c8500b238d15
jonas27 <joE@12345>
2019-07-29 09:56:28 +0200

    Commit from shell script

--- baImplementation/piCoAP/dockerPush.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-export VERSION="v0.0.2arm"
-sudo docker build . -t jbba/picoap:$VERSION
-sudo docker push jbba/picoap:$VERSION
--- baImplementation/piCoAP/test/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mv clienttest.go client_test.go
-go test 
-mv client_test.go clienttest.go
-
